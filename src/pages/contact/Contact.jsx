44b93e12e21902ed368539409c3cebfb634cd8e2
harrypango <harry.pango@gmail.com>
2023-12-11 22:24:30 +0100

    styling changes

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -2,7 +2,32 @@ import React from "react";
 import "./contact.css";
 
 const Contact = () => {
-  return <div>Contact Us NEW</div>;
+  return (
+    <div className="container section__margin">
+      <div className="container-details">
+        <div className="container-details__heading">
+          <h1>CONTACT</h1>
+        </div>
+        <h4>Alter Architects</h4>
+        <p>Tirana, Albania</p>
+        <p>
+          Rruga “Skënderbej” pallati 8 kati përdhe,
+          <br /> Rr. Kavajës <br />
+          1001, Tirana
+        </p>
+        <p>email: info@alter-architects.com</p>
+      </div>
+      <div className="container-form">
+        <div className="container-form__name">
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+        </div>
+        <input type="text" placeholder="E-mail" />
+
+        <textarea type="text" placeholder="Message" />
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
